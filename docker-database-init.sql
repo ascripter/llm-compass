8062a0edf75d250e55f5fac9efa3e3291ef0ac20
Andreas Pfrengle <pfrengle@gmx.net>
2026-02-20 12:38:51 +0100

    cleanup postgres / docker

--- docker-database-init.sql
+++
@@ -1,6 +0,0 @@
--- This runs automatically when container first starts
--- We need to define vector type before SQLAlchemy tries to use it
-CREATE EXTENSION IF NOT EXISTS vector;
-
--- Test it works
-SELECT '[-1,2,3]'::vector;
