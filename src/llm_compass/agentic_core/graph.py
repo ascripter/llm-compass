99c702310d8236d0094b0f106259e846bd7e93b5
Andreas Pfrengle <pfrengle@gmx.net>
2026-03-06 19:43:37 +0100

    graph update

--- src/llm_compass/agentic_core/graph.py
+++ src/llm_compass/agentic_core/graph.py
@@ -8,6 +8,7 @@ from langgraph.checkpoint.memory import MemorySaver
 from .state import AgentState
 from .nodes import (
     validate_intent_node,
+    token_ratio_estimation_node,
     query_refiner_node,
 )
 
@@ -16,7 +17,9 @@ def build_graph():
     workflow = StateGraph(AgentState)
 
     workflow.add_node("validator", validate_intent_node)
+    workflow.add_node("token_ratio", token_ratio_estimation_node)
     workflow.add_node("refiner", query_refiner_node)
+    # workflow.add_node("benchmark_discovery", benchmark_discovery)
     # TODO: add discovery, ranking, synthesis nodes
 
     # Edges
@@ -35,9 +38,10 @@ def build_graph():
             is_specific = bool(intent_extraction.is_specific)
         if is_specific is False:
             return "validator"
-        return "refiner"
+        return ["token_ratio", "refiner"]
 
     workflow.add_conditional_edges("validator", check_validity)
-    workflow.add_edge("refiner", END)
+    # workflow.add_edge(["token_ratio", "refiner"], "benchmark_discovery")
+    
 
     return workflow.compile(checkpointer=MemorySaver())
